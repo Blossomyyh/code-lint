a69e4e70f7e3d79de426d8d8a291c8ee6c5bcdd4
yuhanyin <blossomyyh@163.com>
2019-11-04 19:17:56 -0500

    2

--- src/amazon.java
+++ src/amazon.java
@@ -248,6 +248,47 @@ public class amazon {
     }
 
 
+
+
+    public Map<Integer, Double> highFive(Record[] results) {
+        // Write your code here
+
+        Map<Integer, Double> result = new HashMap<Integer, Double>();
+        HashMap<Integer, PriorityQueue<Record>> map = new HashMap<Integer, PriorityQueue<Record>>();
+
+        int k = 5;
+        for (Record r : results) {
+            if (!map.containsKey(r.id)) {
+                PriorityQueue<Record> pq = new PriorityQueue<Record>(k, new Comparator<Record>() {
+                    public int compare(Record a, Record b) {
+                        return a.score - b.score; // min-heap
+                    }
+                });
+                map.put(r.id, pq);
+            }
+
+            map.get(r.id).add(r);
+            if (map.get(r.id).size() > k) {
+                map.get(r.id).poll();
+            }
+        }
+
+        for (Map.Entry<Integer, PriorityQueue<Record>> entry : map.entrySet()) {
+            int id = entry.getKey();
+            PriorityQueue<Record> pq = entry.getValue();
+            double average = 0;
+            int num = pq.size();
+            while (!pq.isEmpty()) {
+                average += pq.poll().score;
+            }
+            average /= num;
+            result.put(id, average);
+        }
+
+        return result;
+    }
+
+
     public static void main(String args[]){
 //        1.mostcommon words
 //        String m = commonWord("a, AAA, ? ! &% $ # &((a, a, b,b,b,c, c", new String[]{"b"});
@@ -294,4 +335,12 @@ public class amazon {
 
     }
 
+}
+
+class Record {
+    public int id, score;
+    public Record(int id, int score){
+        this.id = id;
+        this.score = score;
+    }
 }
