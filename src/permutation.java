2eda44b595eb69d000c1727d1fff0e49ffe3b3ce
yuhanyin <blossomyyh@163.com>
2019-12-04 15:15:33 -0500

    permutation

--- src/permutation.java
+++ src/permutation.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class permutation {
     static Set< String > set = new HashSet< >();
@@ -61,15 +58,105 @@ public class permutation {
                 set.add(new String(s));
         } else {
             for (int i = l; i < s.length; i++) {
+
+                //first need to swap 2
                 swap(s, l, i);
+                //permute to next step
                 simplepermute(s, l + 1);
+                //have to put back the swap
                 swap(s, l, i);
             }
         }
     }
 
 
+    /**
+     * permute through distinct integer
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> permute(int[] nums) {
+        //unique integer
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> init = new ArrayList<>();
+        for (int num : nums)
+            init.add(num);
+        res = permute(init, 0,nums.length,res);
+
+        return res;
+    }
+
+    public static List<List<Integer>> permute(List<Integer> nums, int k,  int n, List<List<Integer>> output){
+        if(k==n) output.add(new ArrayList<Integer>(nums));
+        for(int i = k;i<n;i++){
+            if(!(nums.get(i)==nums.get(k) && i!=k)) {
+                Collections.swap(nums, k, i);
+
+                permute(nums, k + 1, n, output);
+                //need to put it back!
+                Collections.swap(nums, k, i);
+            }
+        }
+        return output;
+    }
+
+    /**
+     * permute with duplicate integer
+     * @param
+     */
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        //unique integer
+        List<List<Integer>> res = new ArrayList<>();
+        int[] unum = new int[nums.length];
+        int[] fnum = new int[nums.length];
+        int[] num = new int[nums.length];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int m : nums){
+            map.put(m, map.getOrDefault(m,0)+1);
+        }
+        //Map.Entry<Integer, Integer>   map.entrySet()
+        int i = 0;
+        for(HashMap.Entry<Integer, Integer> item: map.entrySet()){
+            unum[i]=item.getKey();
+            fnum[i]=item.getValue();
+            i++;
+        }
+
+
+
+        res = permute(0,num,unum,fnum,res);
+
+        return res;
+    }
+
+    public static List<List<Integer>> permute(int h, int[] num, int[] unum,  int[] fnum, List<List<Integer>> output){
+        if(h==num.length){
+            List<Integer> list = new ArrayList<>();
+            for(int n: num) list.add(n);
+            output.add(list);
+        }else{
+            for(int i = 0;i<unum.length;i++){
+                if(fnum[i]<1) continue;
+                else{
+                    num[h] = unum[i];
+                    fnum[i]--;
+                    permute(h+1,num, unum,fnum, output);
+                    //need to put it back!
+                    fnum[i]++;
+                }
+            }
+        }
+
+        return output;
+    }
+
+
+
+
     public static void main (String args[]){
-        generatePalindromes("aabbccd");
+        int[] nums= new int[]{2,2,1,1};
+        permuteUnique(nums);
+//        generatePalindromes("aabbccd");
     }
 }
