1f509976fc30d9030b191d654157fd549bda0563
yuhanyin <blossomyyh@163.com>
2020-01-14 22:23:57 -0500

    tree

--- src/treeOrder.java
+++ src/treeOrder.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class treeOrder {
     public static List<Integer> TreeOrder(TreeNode root){
@@ -16,7 +17,7 @@ public class treeOrder {
      * todo: Preorder : root left right
      * todo: Postorder: left right root
      *
-     * 
+     *
      * @param res
      * @param node
      * @return
@@ -29,6 +30,51 @@ public class treeOrder {
         res.add(node.val);
 
         res = inorder(res, node.right);
+        return res;
+    }
+
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> res = new ArrayList<>();
+        //if(root== null) return res;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur!=null || !stack.isEmpty() ){
+            while(cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+
+        return res;
+    }
+
+
+    /**
+     * post order
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(root == null) return res;
+        Stack<TreeNode> stack= new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if(node.right!=null)
+                stack.push(node.right);
+            if(node.left!= null)
+                stack.push(node.left);
+        }
         return res;
     }
 }
